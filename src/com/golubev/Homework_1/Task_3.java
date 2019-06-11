3cc3a1073b30168681c61af57186b29bd91ae68f
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-11 16:04:24 +0300

    Update Task_3.java

--- src/com/golubev/Homework_1/Task_3.java
+++ src/com/golubev/Homework_1/Task_3.java
@@ -1,7 +1,12 @@
 package com.golubev.Homework_1;
 
+/*
+Сделай три раздельных if. Будет гораздо читабельнее 
+Ты в каждом ифе выводишь на экран. Зачем? Можно один раз в конце (закоментированный код)
+*/
 public class Task_3 {
     public static void main(String[] args) {
+        // имена переменных с маленькой буквы
         int Number1 = 0;
         if (Number1 > 0) {
             Number1++;
@@ -13,10 +18,12 @@ public class Task_3 {
                 System.out.print(Number1);
             }
             else {
+                // не выводи просто 10. Присвой значение переменной и выводи переменную
                 System.out.print(10);
             }
 
 
         }
+        // System.out.print(Number1); <- одного раза достаточно.
     }
 }
