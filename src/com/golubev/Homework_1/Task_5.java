180462f6b24ada0269e4f0bb414ff985a7b484a1
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-11 16:11:21 +0300

    Update Task_5.java

--- src/com/golubev/Homework_1/Task_5.java
+++ src/com/golubev/Homework_1/Task_5.java
@@ -5,13 +5,16 @@ public class Task_5 {
         int Number1=-2;
         int Number2=-2;
         int Number3=-3;
+        
+        // где переменную используешь, там ее и заводи. Т.е. на строку 28 ее надо. Второе - имя переменной,quantityOfPositiveNumbers
+         // называй переменную на русском, затем октрывай гугл переводчик и переводи. Со временем будет проще
         int Number;
         int NumberA=0;
         if (Number1>0) {
             Number1=1;
 
         }
-        else {Number1=0; NumberA+=1;}
+        else {Number1=0; NumberA+=1;} // зачем ты меняешь значение базовой переменной? не надо
         if (Number2>0) {
             Number2=1;
 
@@ -23,6 +26,7 @@ public class Task_5 {
         else {
             Number3=0; NumberA+=1;}
         Number=Number1+Number2+Number3;
+        
         System.out.println("Количество положительных чисел "+Number);
         System.out.print("Количество отрицательных чисел " + NumberA);
 
